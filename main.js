0b9c699263f90aef5e96f19700eada5dcda13f98
ecwebtests <ecwebbtesting@gmail.com>
2026-04-23 23:47:28 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 //HOME PAGE HERO SECTION
 const sliderContainer =document.querySelector('.slider');
 const slides=document.querySelectorAll('.slide');
@@ -35,3 +38,24 @@ function  AutoRotateSlides(){
 }
 
 AutoRotateSlides();
+
+
+//CHANGING TEXT ON CALL TO ACTION BUTTON
+const words =["Success","Excellence","Brilliance"];
+let indexx=0;
+const changeText=document.getElementById('changingText');
+
+    function changeWord(){
+      changeText.style.opacity=0;
+      setTimeout(()=>{
+        changeText.textContent=words[indexx];
+        changeText.style.opacity=1;
+        indexx++;
+
+        if(indexx>=words.length){
+          indexx=0;
+        }
+      },400)
+    }
+    changeWord()
+setInterval(changeWord,4000)
